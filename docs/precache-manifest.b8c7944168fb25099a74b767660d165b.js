f83667cd07321c24868f7222619d6604010ba90c
Alba San Martin <alba.sanmartin.g@gmail.com>
2020-10-07 19:21:24 +0200

    add docs folder

---
+++ docs/precache-manifest.b8c7944168fb25099a74b767660d165b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bef2241f9373a4f0343ad3a889cb88ca",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3aa19d32f4f33c960de1",
+    "url": "./static/css/main.b41a2f60.chunk.css"
+  },
+  {
+    "revision": "bf44161d267ef1b84c04",
+    "url": "./static/js/2.fecb6cc9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.fecb6cc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aa19d32f4f33c960de1",
+    "url": "./static/js/main.3254943e.chunk.js"
+  },
+  {
+    "revision": "23bedb808eb06b70b3c7",
+    "url": "./static/js/runtime-main.1ddce69c.js"
+  }
+]);
