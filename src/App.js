7ec829c35e594f574da121d27d0659a5264dce03
Alba San Martin <alba.sanmartin.g@gmail.com>
2020-10-07 16:46:54 +0200

    add board

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import React from 'react';
 
-function App() {
-  return <div className="App">Hola</div>;
-}
+const createBoard = () => {
+  const board = [];
+
+  for (let row = 0; row < 2; row++) {
+    board[row] = [];
+    for (let col = 0; col < 3; col++) {
+      board[row] = [...board[row], 'A!'];
+    }
+  }
+
+  board[2] = ['', '', ''];
+
+  return board;
+};
+
+const App = () => {
+  const board = createBoard();
+
+  return (
+    <div className="App">
+      <div className="board">
+        {board.map((row) => (
+          <div className="board__row">
+            {row.map((col) => (
+              <div className="board__col">{col}</div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default App;
