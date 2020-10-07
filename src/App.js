5debb04b333461d96a03aae21528b87f5b39ee09
Alba San Martin <alba.sanmartin.g@gmail.com>
2020-10-07 16:53:58 +0200

    add random rows

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
+const getColumnValue = () => {
+  const columnValues = ['O', 'X'];
+  const randomNumber = Math.round(Math.random());
+
+  return columnValues[randomNumber];
+};
+
 const createBoard = () => {
   const board = [];
 
   for (let row = 0; row < 2; row++) {
     board[row] = [];
     for (let col = 0; col < 3; col++) {
-      board[row] = [...board[row], 'A!'];
+      board[row] = [...board[row], getColumnValue()];
     }
   }
 
@@ -22,9 +29,9 @@ const App = () => {
     <div className="App">
       <div className="board">
         {board.map((row) => (
-          <div className="board__row">
+          <div className="row">
             {row.map((col) => (
-              <div className="board__col">{col}</div>
+              <div className="col">{col}</div>
             ))}
           </div>
         ))}
